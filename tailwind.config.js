1a1cbe17b58d7787b71ef168cb71e05bcbbfc2cd
alamindev <mdalamin6554@gmail.com>
2022-06-16 14:23:17 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'dm': ["DM Sans", "sans-serif"], 
+    },
+    extend: {
+      colors: { 
+      },
+    },
+    container: { 
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1132px", 
+      },
+    },
+  },
+  plugins: [],
+};
